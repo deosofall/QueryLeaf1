8face63f683fc406efd6c45c96b0d2a22fee603b
Aparnesh Singh <aparneshsingh74@gmail.com>
2026-08-17 14:11:25 +0530

    Use S3Client and GetObjectCommand directly to prevent s3:ListBucket permission error

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -1,4 +1,4 @@
-import { S3 } from "@aws-sdk/client-s3";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import fs from "fs";
 import path from "path";
 import os from "os";
@@ -7,42 +7,37 @@ export async function downloadFromS3(file_key: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
       const region = process.env.NEXT_PUBLIC_S3_REGION || "eu-north-1";
-      const s3 = new S3({
+      const s3Client = new S3Client({
         region,
         credentials: {
           accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
           secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!,
         },
       });
-      const params = {
+      const command = new GetObjectCommand({
         Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
         Key: file_key,
-      };
+      });
 
-      const obj = await s3.getObject(params);
+      const obj = await s3Client.send(command);
       const tmpDir = os.tmpdir();
       if (!fs.existsSync(tmpDir)) {
         fs.mkdirSync(tmpDir, { recursive: true });
       }
       const file_name = path.join(tmpDir, `queryleaf-${Date.now().toString()}.pdf`);
 
-      if (obj.Body instanceof require("stream").Readable) {
-        // AWS-SDK v3 has some issues with their typescript definitions, but this works
-        // https://github.com/aws/aws-sdk-js-v3/issues/843
-        //open the writable stream and write the file
-        const file = fs.createWriteStream(file_name);
-        file.on("open", function (fd) {
-          // @ts-ignore
-          obj.Body?.pipe(file).on("finish", () => {
-            return resolve(file_name);
-          });
-        });
-        // obj.Body?.pipe(fs.createWriteStream(file_name));
+      if (obj.Body) {
+        const fileStream = fs.createWriteStream(file_name);
+        const stream = obj.Body as any;
+        stream.pipe(fileStream);
+        stream.on("finish", () => resolve(file_name));
+        stream.on("error", (err: any) => reject(err));
+      } else {
+        reject(new Error("Empty body received from S3"));
       }
     } catch (error) {
-      console.error(error);
+      console.error("Error downloading from S3:", error);
       reject(error);
-      return null;
     }
   });
 }
